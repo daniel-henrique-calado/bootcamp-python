680397a46aa945bace806c78fdfb78a38a5e203f
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 19:44:54 -0300

    Finishing exercise 03

---
+++ aula_02/src/exercises/exercise_03.py
@@ -0,0 +1,7 @@
+"""
+Receives two integers numbers from user interface and print out the product.
+"""
+num_01 = int(input("Informe um número: "))
+num_02 = int(input("Informe outro número: "))
+
+print(f"O produto do {num_01} por {num_02} é {num_01 * num_02}")
