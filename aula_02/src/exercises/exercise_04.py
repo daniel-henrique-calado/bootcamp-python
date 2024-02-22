4b3eebe74b5573782ccda25c8990d11b832a8315
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 20:03:11 -0300

    Adding exercise 4

---
+++ aula_02/src/exercises/exercise_04.py
@@ -0,0 +1,9 @@
+"""
+Read two numbers from user and print out integer division 
+between the first and second number on this order.
+"""
+
+num_01 = int(input("Informe um número: "))
+num_02 = int(input("Informe outro número: "))
+
+print(f"A divisão {num_01} por {num_02} é {num_01//num_02}")
