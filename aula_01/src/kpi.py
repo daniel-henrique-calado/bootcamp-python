a6359f5d41ca7bd9ed6d74abbc54f0493b1074ba
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 18:09:49 -0300

    KPI ajust in a constant

--- aula_01/src/kpi.py
+++ aula_01/src/kpi.py
@@ -3,11 +3,13 @@ Receives a user name, his salary and a bonification value percent.
 Uses the following expression to calculate his salary bonus.
 salary_bonus: 1000 + salary * bonus_percent
 """
+CONSTANT = 1000
+
 user_name = input("Digite seu nome: ")
 salary = float(input("Informe seu salário mensal : R$ "))
 bonus_percent = float(input("Informe seu bônus percentual: "))
 
-salary_bonus = 1000 + salary * bonus_percent
+salary_bonus = CONSTANT + salary * bonus_percent
 
 print(f"Olá {user_name}, seu bônus foi de {salary_bonus}")
 
