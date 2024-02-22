54f4d5d1617ec85ef57d63bae74dbf3377b4ae52
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 19:30:15 -0300

    Editing output of exercise 1 and creating exercise 02

--- aula_02/src/exercises/exercise_01.py
+++ aula_02/src/exercises/exercise_01.py
@@ -4,4 +4,4 @@ Receives two integers numbers and print out sum.
 num_01 = int(input("Digite um valor: "))
 num_02 = int(input("Digite outro valor: "))
 
-print(f"{num_01 + num_02}")
+print(f"Soma de {num_01} e {num_02} é {num_01 + num_02}")
---
+++ aula_02/src/exercises/exercise_02.py
@@ -0,0 +1,7 @@
+"""
+Receives a integer number and print a mod operation by 5
+"""
+CONSTANT = 5
+number = int(input("Digite um número: "))
+
+print(f"Resto da divisão de {number} por {CONSTANT} é: {number % CONSTANT}")
