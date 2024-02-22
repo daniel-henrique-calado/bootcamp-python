9fe00e6c85dfea65f1e14669a0b8095001577ce8
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 17:44:35 -0300

    Exercise 2 resolution

---
+++ aula_01/src/exercises/exercise_02.py
@@ -0,0 +1,7 @@
+"""
+Read two values from user interface and printout the sum.
+"""
+num_01 = float(input("Digite um valor: "))
+num_02 = float(input("Digite outro valor: "))
+
+print(f"{num_01 + num_02:.0f}")
