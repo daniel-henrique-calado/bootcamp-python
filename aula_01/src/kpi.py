ad1f158fb3cb5238aa425cc291f0fda75044ab53
Daniel Calado <daniel.calado@accenture.com>
2024-02-22 18:02:21 -0300

    KPI calculation

---
+++ aula_01/src/kpi.py
@@ -0,0 +1,13 @@
+"""
+Receives a user name, his salary and a bonification value percent.
+Uses the following expression to calculate his salary bonus.
+salary_bonus: 1000 + salary * bonus_percent
+"""
+user_name = input("Digite seu nome: ")
+salary = float(input("Informe seu salário mensal : R$ "))
+bonus_percent = float(input("Informe seu bônus percentual: "))
+
+salary_bonus = 1000 + salary * bonus_percent
+
+print(f"Olá {user_name}, seu bônus foi de {salary_bonus}")
+
