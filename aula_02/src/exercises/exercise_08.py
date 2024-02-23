d9ac79b74b13583c0fcaf0e9b05fd8b29fa9e4b6
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 19:22:00 -0300

    Adding exercise 8

---
+++ aula_02/src/exercises/exercise_08.py
@@ -0,0 +1,10 @@
+"""
+Calculate the power between two numbers given by user. 
+The first number must be the base and second the exponent.
+"""
+
+
+num_01 = float(input("Informe um número: "))
+num_02 = float(input("Informe outro número: "))
+
+print(f"O número {num_01} elevado a {num_02} resulta em: {num_01 ** num_02}")
