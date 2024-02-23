1dc88883e7899789de41489d2907febda39bd171
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 19:27:41 -0300

    Adding exercise 9

---
+++ aula_02/src/exercises/exercise_09.py
@@ -0,0 +1,8 @@
+"""
+Convert a Celsius temperature to Fahrenheit scale.
+"""
+
+temperature_celsius = float(input("Informe a temperatura em °C: "))
+temperature_fahrenheit = temperature_celsius * 1.8 + 32
+
+print(f"A temperatura {temperature_celsius} °C equivale a {temperature_fahrenheit} °F.")
