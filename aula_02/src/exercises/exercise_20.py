a3eec0f5e8c01b8c56dcdf1ed1b666fc19e6ea3a
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 20:26:37 -0300

    Adding exercise 20

---
+++ aula_02/src/exercises/exercise_20.py
@@ -0,0 +1,8 @@
+"""
+Check if two numbers are different.
+"""
+
+num_01 = int(input("Digite um número: "))
+num_02 = int(input("Digite outro número: "))
+
+print(f"A desigualdade entre os números {num_01} e {num_02} é: {num_01 != num_02}")
