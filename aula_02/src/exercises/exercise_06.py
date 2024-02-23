c5505630adafdf583023796b2cc467fccef651e1
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 19:10:23 -0300

    Adding exercise 6

---
+++ aula_02/src/exercises/exercise_06.py
@@ -0,0 +1,8 @@
+"""
+Calculate the sum of two float numbers.
+"""
+
+num_01 = float(input("Digite um número: "))
+num_02 = float(input("Digite outro número: "))
+
+print(f"A soma dos números {num_01} e {num_02}: é {num_01 + num_02}")
