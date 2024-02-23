b9036c3ae10cc0e8444cc64b39d0d5834e167204
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 20:20:26 -0300

    Adding exercise 18

---
+++ aula_02/src/exercises/exercise_18.py
@@ -0,0 +1,7 @@
+"""
+Apply a NOT operator on a boolean value.
+"""
+
+boolean = bool(int(input("Informe um booleano (0 ou 1): ")))
+
+print(f"A operação AND para os dois booleanos é: {not boolean}")
