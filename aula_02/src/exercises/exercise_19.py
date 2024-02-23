697530089597352417b65243675aa20845911f3d
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 20:25:26 -0300

    Adding exercise 19

---
+++ aula_02/src/exercises/exercise_19.py
@@ -0,0 +1,8 @@
+"""
+Check if two numbers are equal.
+"""
+
+num_01 = int(input("Digite um número: "))
+num_02 = int(input("Digite outro número: "))
+
+print(f"A igualdade entre os números {num_01} e {num_02} é: {num_01 == num_02}")
