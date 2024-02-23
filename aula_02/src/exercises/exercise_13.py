e8227d92573a85cf49d4bfa35b7e90aabe238f1d
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 19:50:01 -0300

    Adding exercise 13

---
+++ aula_02/src/exercises/exercise_13.py
@@ -0,0 +1,7 @@
+"""
+Remove blank space at the beginning and end of a given phrase.
+"""
+
+phrase = input("Digite uma frase: ")
+
+print(f"Os espaços no inicio e fim da frase foram removidos: {phrase.strip()}")
