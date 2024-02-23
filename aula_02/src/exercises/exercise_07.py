4d9ba65b1403f75200aca572c525dd4caa710352
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-23 19:16:20 -0300

    Adding exercise 7

---
+++ aula_02/src/exercises/exercise_07.py
@@ -0,0 +1,10 @@
+"""
+Calculate the mean between two float numbers.
+"""
+QUANTITY_NUMBERS = 2
+
+num_01 = float(input("Digite um número: "))
+num_02 = float(input("Digite outro número: "))
+
+print(f"A média entre os números {num_01} e {num_02} é: {(num_01 + num_02)/QUANTITY_NUMBERS}")
+
