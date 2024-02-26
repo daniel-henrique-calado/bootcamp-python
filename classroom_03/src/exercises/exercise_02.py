27da8d9ab74a28fa14edeb186c15ae4b4229ae6b
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-25 21:27:15 -0300

    Adding exercise 2

---
+++ classroom_03/src/exercises/exercise_02.py
@@ -0,0 +1,21 @@
+"""
+Classify a sensor temperature based on the following rules:
+- LOW < 18°C;
+- NORMAL >= 18° and <= 26°C
+- HIGH > 26°C
+"""
+
+LOW_TEMPERATURE = 18
+HIGH_TEMPERATURE = 26
+
+try:
+    temperature = float(input("Sensor temperature: "))
+
+    if temperature < LOW_TEMPERATURE:
+        print("Low.")
+    elif temperature >= LOW_TEMPERATURE and temperature <= HIGH_TEMPERATURE:
+        print("Normal.")
+    else:
+        print("High.")
+except ValueError as e:
+    print("Invalid data.")
