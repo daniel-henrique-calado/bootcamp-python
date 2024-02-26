4ff4bd5205bd69b30888c95159c17a58868c06a6
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-26 18:25:39 -0300

    Adding exercise 6

---
+++ classroom_03/src/exercises/exercise_06.py
@@ -0,0 +1,16 @@
+"""
+Detect how many times a word apears in phrase.
+"""
+
+text = "The brown fox jumps over a lazy dog." 
+
+words = text.split()
+count_words = {}
+
+for word in words:
+    if word in count_words:
+        count_words[word] += 1
+    else:
+        count_words[word] = 1
+
+print(count_words)
